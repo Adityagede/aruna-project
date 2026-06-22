c3d565f376647678e770483f84cf83796804f0b5
Gede-aditya <gedeaditya110@gmail.com>
2026-06-22 10:44:22 +0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,49 @@
+import './style.css'
+
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+
+import Swiper from 'swiper'
+import { Navigation, Pagination, Autoplay } from 'swiper/modules'
+
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+
+AOS.init({
+  duration: 800,
+  easing: 'ease-out',
+  once: true,
+  offset: 100,
+})
+
+const projectSwiper = new Swiper('.project-swiper', {
+  modules: [Navigation, Pagination, Autoplay],
+  slidesPerView: 1,
+  spaceBetween: 24,
+  loop: true,
+
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+})
