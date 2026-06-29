88edbd3384c65c36ccc4e5331d112b92bc88e83e
Gede-aditya <gedeaditya110@gmail.com>
2026-06-29 14:07:24 +0800

    29juni pm

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,20 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'node:path'
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+
   base: '/aruna-project/',
+
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(import.meta.dirname, 'index.html'),
+        about: resolve(import.meta.dirname, 'about.html'),
+      },
+    },
+  },
 })
